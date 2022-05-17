40a8424675410b4297f84260a847ef3624146504
Lightning-g944 <92637945+Lightning-g944@users.noreply.github.com>
2022-05-17 21:05:06 +0530

    Update Impala_Queries.sql
    updated the the comments with correct character for commenting.

--- Impala_Queries.sql
+++ Impala_Queries.sql
@@ -2,7 +2,7 @@ CREATE database Capstone_Hadoop_RK;
 
 USE Capstone_Hadoop_RK;
 
-##1. A List showing employee number, last_name, first name, sex and salary for each employee.
+--1. A List showing employee number, last_name, first name, sex and salary for each employee.
 SELECT 
     e.emp_no, 
     e.last_name, 
@@ -11,7 +11,7 @@ SELECT
 FROM employees e 
 INNER JOIN salaries s on s.emp_no = e.emp_no;
 
-##2. A list showing first name, last_name and hire date for employees who were hired in 1986.
+--2. A list showing first name, last_name and hire date for employees who were hired in 1986.
 select
 	first_name,
 	last_name,
@@ -19,8 +19,8 @@ select
 from employees
 where year(to_date(unix_time(unix_timestamp(hire_date, 'dd/MM/yyyy')))) = 1986;
 
-##3. A list showing the manager of each department with the following information: department number, 
-##	department name, the manager's employee number, last name, first name.
+--3. A list showing the manager of each department with the following information: department number, 
+--	department name, the manager's employee number, last name, first name.
 SELECT 
     d.dept_no, 
     d.dept_name, 
@@ -33,8 +33,8 @@ ON d.dept_no = dm.dept_no
 JOIN employees e
 ON dm.emp_no = e.emp_no;
 
-##4. List the department of each employee with the following information: 
-##	employee number, last name, first name, and department name.
+--4. List the department of each employee with the following information: 
+--	employee number, last name, first name, and department name.
 SELECT
     de.emp_no,
     e.last_name,
@@ -46,8 +46,8 @@ ON de.emp_no = e.emp_no
 JOIN departments d
 ON de.dept_no = d.dept_no;
 
-##5. A list showing first name, last name, and sex of employees whose first name is 'Hercules'
-##	and last name begin with 'B'
+--5. A list showing first name, last name, and sex of employees whose first name is 'Hercules'
+--	and last name begin with 'B'
 SELECT
     first_name,
     last_name,
@@ -56,8 +56,8 @@ FROM employees
 WHERE first_name = 'Hercules' 
 AND last_name LIKE 'B%';
 
-##6. A list showing all employees in the Sales department, including their employee number, last name,
-##	first name, department name.
+--6. A list showing all employees in the Sales department, including their employee number, last name,
+--	first name, department name.
 SELECT
     de.dept_no,
     e.last_name,
@@ -70,8 +70,8 @@ JOIN
 departments d on de.dept_no = d.dept_no
 WHERE d.dept_name = '"Sales"';
 
-##7. A list showing all employees in the sales and development departments including their employee number,
-##	last name, first name and department name.
+--7. A list showing all employees in the sales and development departments including their employee number,
+--	last name, first name and department name.
 SELECT
     de.emp_no,
     e.last_name,
@@ -84,8 +84,8 @@ JOIN
 departments d on de.dept_no = d.dept_no
 WHERE d.dept_name = '"Sales"' OR d.dept_name = '"development"';
 
-##8. A list showing the frequency count of employee last name, in descending order (i.e., how many
-##		employees share each last name)
+--8. A list showing the frequency count of employee last name, in descending order (i.e., how many
+--		employees share each last name)
 SELECT
     count(last_name) as Frequency
 FROM employees
